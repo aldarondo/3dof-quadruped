3bdbf394d41524d19bf794f9ce70e2158e8a51fb
Charles Aldarondo <charles.aldarondo@gmail.com>
2016-08-20 05:57:29 -0700

    Initial Version

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,20 @@
+/*
+ * Arduino.ino
+ *
+ * Created: 8/14/2014 2:36:59 PM
+ * Author: caldarondo
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 ==============
 
 3DOF Quadruped Robot
+
+
+Software Tools
+=================
+
+AtmelStudio 6 (Recommended) or Arduino IDE
+
+
+Bill Of Materials
+=================
+* = Optional
